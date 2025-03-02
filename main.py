9fd1d924304aed02db77971312c94b5f7a46c466
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-02 17:52:21 +0800

    feat: implement document reading route

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+from datetime import datetime
+import logging
+import os
+from fastapi import FastAPI, UploadFile
 import cv2
 
-img = cv2.imread('/')
+server = FastAPI()
+
+LOGGER = logging.getLogger(__name__)
+
+@server.post('/document/')
+async def signatures(file: UploadFile):
+    filename = f"dump/{datetime.now()}-{file.filename}"
+    image_file = open(filename, "xb+")
+    data = await file.read()
+    image_file.write(data)
+    image_file.flush()
+    img = cv2.imread(filename)
+    qrd = cv2.QRCodeDetector()
+    retval, decoded_info, _, _ = qrd.detectAndDecodeMulti(img)
+
+    os.remove(filename)
+    return {
+        retval,
+        decoded_info
+    }
